436b1547195b9ac30f890fb55f000d26720900a3
Mohamed <blee87202@gmail.com>
2026-02-19 05:40:40 +0200

    Update my_orders.php

--- Controller/my_orders.php
+++ Controller/my_orders.php
@@ -1,11 +1,9 @@
 <?php
-// my_orders.php - Main Controller
 session_start();
-require_once '../Vendor/autoload.php'; // Assuming Smarty is loaded via Composer
-require_once '../Smarty/libs/Smarty.class.php'; // Or direct Smarty inclusion
-require_once '../Model/db.php'; // Database connection
+require_once '../Vendor/autoload.php'; 
+require_once '../Smarty/libs/Smarty.class.php'; 
+require_once '../Model/db.php'; 
 
-// Check authentication
 if (!isset($_SESSION['user_id'])) {
     header('Location: login.php?redirect=my_orders');
     exit;
@@ -13,15 +11,12 @@ if (!isset($_SESSION['user_id'])) {
 
 $user_id = (int)$_SESSION['user_id'];
 
-// Initialize Smarty
 $smarty = new Smarty();
 $smarty->setTemplateDir('../Views/');
 $smarty->setCompileDir('../Templates_c/');
 $smarty->setCacheDir('../Cache/');
 
-// Assign user data to Smarty if needed
 $smarty->assign('user_id', $user_id);
 
-// Display the template
 $smarty->display('my_orders.html');
 ?>
