5386f64386c02ef5ec2abb65e8da6a031c5e0d2d
Mohamed <blee87202@gmail.com>
2026-02-19 05:43:28 +0200

    Update db.php

--- Model/db.php
+++ Model/db.php
@@ -1,8 +1,6 @@
 <?php
-// ملف db.php المصحح
 $conn = new mysqli("localhost", "root", "", "ecommerce_db");
 if ($conn->connect_error) {
   die("فشل الاتصال: " . $conn->connect_error);
 }
-$conn->set_charset("utf8"); // ✅ مهم جداً لدعم العربية
-// لا يوجد وسم إغلاق لـ PHP لتجنب المسافات البيضاء غير المرغوبة
+$conn->set_charset("utf8");
